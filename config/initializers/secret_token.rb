0b816a0baacd419ae4f5264aa629140ad53f2adf
Paul Zaich <pzaich@gmail.com>
2012-07-29 15:02:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikerack::Application.config.secret_token = '4fa8f9d4bb260392c1ea2fe7930efde5241d1926ac52d782967f0e22319bd3d63b36142cd1d5bc5adb332af6ac0961160b7c062ca230be74263100309d15f00d'
